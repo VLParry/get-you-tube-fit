c756373038e3916b63929db263988d3115d67ff6
Victoria Parry <vlparry@gmail.com>
2023-04-22 13:52:14 -0600

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+    User.create(
+      name: "User #{i+1}",
+      email: "user#{i+1}@example.com",
+      password_digest: "password"
+    )
+  end
+
+
+Workout.create(title: "Bodyweight Cardio Workout with High and Low Impact Modifications", description: "I found this to be an excellent workout", warmup: true, cooldown: true, duration: 30, video_url: "https://https://www.youtube.com/watch?v=5mCcUndJMo8&t=916s", user_id: 1)
+
+Workout.create(title: "HIIT Workout", description: "A high-intensity interval training routine for a full-body workout",warmup: true, cooldown: true, duration: 45, video_url: "https://www.youtube.com/watch?v=ml6cT4AZdqI", user_id: 2)
+
+Workout.create(title: "Cardio Kickboxing Workout", description: "25 Minute Kickboxing Cardio Interval Workout", warmup: true, cooldown: true, duration: 25, video_url: "https://https://www.youtube.com/watch?v=Vve4BVTZ0QU", user_id: 1)
