62c261879cc11392595ab713ca4a0aba067633a0
Victoria Parry <vlparry@gmail.com>
2023-04-22 13:52:00 -0600

    update workout model validations

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,11 @@
 class Workout < ApplicationRecord
   belongs_to :user
+  has_many :workout_tags
+  has_many :tags, through: :workout_tags
+
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :description, presence: true, uniqueness: true, length: { maximum: 200 }
+  validates :duration, presence: true, numericality: { greater_than: 0 }
+  validates :video_url, presence: true, uniqueness: true, format: { with: /\Ahttps?:\/\/[\S]+\z/i, message: "must be a valid URL" }
+  validates :user_id, presence: true
 end
