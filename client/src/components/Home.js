bde4352c0ed4fb0dc8d079461ca0f8ab6dc11ca3
Victoria Parry <vlparry@gmail.com>
2023-04-25 17:07:04 -0600

    create home and user workout

---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useUserContext } from '../context/UserContext';
+
+
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
---
+++ client/src/components/UserWorkouts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useUserContext } from '../context/UserContext';
+
+
+const UserWorkouts = ( ) => {
+  return (
+    
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserWorkouts
