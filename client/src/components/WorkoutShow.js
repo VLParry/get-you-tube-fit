4e3b4e2eae55bdbfacacae007d42b2efcd6d2386
Victoria Parry <vlparry@gmail.com>
2023-05-07 13:41:20 -0400

    set up show page for workouts

---
+++ client/src/components/WorkoutShow.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import YouTube from 'react-youtube';
+import { Card, CardContent, Typography, Grid } from "@mui/material";
+
+const WorkoutShow = () => {
+    const [workout, setWorkout] = useState(null);
+    const { id } = useParams();
+  
+    useEffect(() => {
+      fetch(`/workouts/${id}`)
+        .then((response) => response.json())
+        .then((data) => setWorkout(data));
+    }, [id]);
+  
+    if (!workout) {
+      return <div>Loading...</div>;
+    }
+  
+    const { title, description, video_url } = workout;
+    const opts = {
+      height: '360',
+      width: '640',
+      playerVars: {
+        autoplay: 0,
+      },
+    };
+  
+    const getVideoId = (videoUrl) => {
+      const pattern = /youtube.com\/watch\?v=(\w+)/;
+      const match = videoUrl.match(pattern);
+      return match ? match[1] : '';
+    };
+  return (
+        <Grid container spacing={2} justifyContent="center">
+    <Grid item xs={12} md={8}>
+      <Card>
+        <CardContent>
+          <Typography variant="h3">{workout.title}</Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            {workout.description}
+          </Typography>
+          <Typography variant="body1">
+            Duration: {workout.duration} minutes
+          </Typography>
+          <Typography variant="body1">
+            Warmup? {workout.warmup ? 'Yes' : 'No'}
+          </Typography>
+          <Typography variant="body1">
+            Cooldown? {workout.cooldown ? 'Yes' : 'No'}
+          </Typography>
+          <div style={{ marginTop: 16 }}>
+            <YouTube videoId={getVideoId(workout.video_url)} opts={opts} />
+          </div>
+        </CardContent>
+      </Card>
+    </Grid>
+  </Grid>
+  )
+}
+
+export default WorkoutShow
