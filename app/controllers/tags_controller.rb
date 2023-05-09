20a40f6e26bef29368393c03509dfe5391fa82ae
Victoria Parry <vlparry@gmail.com>
2023-05-09 12:16:36 -0400

    update tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,24 @@
 class TagsController < ApplicationController
+    skip_before_action :authorized, only: [:index, :show]
+
+    def index
+        render json: Tag.all
+    end
+
+    def show
+        tag = Tag.find(params[:id])
+        render json: tag
+    end
+
+    def create  
+        tag = Tag.create!(tag_params)
+        render json: tag, status: :created
+    end
+
+    private
+
+    def tag_params
+        params.permit(:name)
+    end
+
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
     has_many :workout_tags
     has_many :workouts, through: :workout_tags
+
+    validates :name, presence: true, uniqueness: true
 end
