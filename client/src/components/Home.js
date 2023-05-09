2b9027c1c3085e66af9e5f96d7c9b1942a53c039
Victoria Parry <vlparry@gmail.com>
2023-05-09 12:17:30 -0400

    set up home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,37 @@
-import React from 'react'
-import { useUserContext } from '../context/UserContext';
+import React, { useEffect, useState } from 'react'
+// import { useUserContext } from '../context/UserContext';
+import YouTube from 'react-youtube';
 
 
-const Home = () => {
+
+const Home = ( {workouts} ) => {
+  const [randomWorkout, setRandomWorkout] = useState(null)
+
+  useEffect(() => {
+    if (workouts.length > 0) {
+      const random = Math.floor(Math.random() * workouts.length)
+      setRandomWorkout(workouts[random])
+    }
+  }, [workouts])
+
+console.log("randomworkout", randomWorkout)
+
+const getVideoId = (videoUrl) => {
+  const pattern = /youtube.com\/watch\?v=(\w+)/;
+  const match = videoUrl.match(pattern);
+  return match ? match[1] : '';
+}
+
   return (
     <div>
-      
+      <h1>A workout we love!</h1>
+      {randomWorkout && (
+        <div>
+          <p>{randomWorkout.title}</p>
+          <YouTube videoId={getVideoId(randomWorkout.video_url)} />
+        </div>
+          
+      )}
     </div>
   )
 }
