6ea54450821d818c961dc9bcfb78b66f49d4e910
Victoria Parry <vlparry@gmail.com>
2023-05-09 12:18:59 -0400

    set up workout list page

--- client/src/components/Workouts.js
+++ client/src/components/Workouts.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import YouTube from 'react-youtube';
+import { Grid, Card, CardContent, Typography } from '@mui/material';
+import AddWorkout from './AddWorkout';
+
+//should I pass the workout state and addworkout from App or ok to go through workouts?
 
 
-
-const Workouts = ( {workouts, setWorkouts} ) => {
+const Workouts = ( {workouts, setWorkouts, handleAddWorkout} ) => {
     const opts = {
         height: '360',
         width: '640',
@@ -20,8 +23,12 @@ const Workouts = ( {workouts, setWorkouts} ) => {
   
   return (
     <div>
-
-        <ul>
+      <AddWorkout 
+      key={workouts.id}
+      workout={workouts}
+      setWorkouts={setWorkouts}
+      handleAddWorkout={handleAddWorkout} />
+        {/* <ul>
       {workouts.map(workout => (
 
     <li key={workout.id}>
@@ -32,7 +39,25 @@ const Workouts = ( {workouts, setWorkouts} ) => {
       
     </li>
 ))}
-</ul>
+</ul> */}
+    <Grid container spacing={2}>
+      {workouts.map((workout) => (
+        <Grid key={workout.id} item xs={12} sm={6} md={4}>
+          <Card>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+              <a href={`/workouts/${workout.id}`}>{workout.title}</a>              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {workout.description}
+              </Typography>
+              <YouTube videoId={getVideoId(workout.video_url)} opts={opts} />
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+
+
     </div>
   )
 }
