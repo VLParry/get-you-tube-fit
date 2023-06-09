7804ec1cc7b0cf4adbaf7cf5a9896a4cbdcfbf31
Victoria Parry <vlparry@gmail.com>
2023-06-09 13:10:59 -0400

    add user id

--- app/serializers/workout_serializer.rb
+++ app/serializers/workout_serializer.rb
@@ -1,4 +1,4 @@
 class WorkoutSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :warmup, :cooldown, :duration, :video_url, :tags
+  attributes :id, :title, :description, :warmup, :cooldown, :duration, :video_url, :tags, :user_id
   has_one :user
 end
