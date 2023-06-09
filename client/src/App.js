7ae761869d11bf5b599309f7a62fa8dc0e1057ec
Victoria Parry <vlparry@gmail.com>
2023-06-09 13:12:48 -0400

    update handle edit and delete

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import TagShow from './components/TagShow';
 
 
 function App() {
-  const {setUser} = useUserContext()
+  const {setUser, user} = useUserContext()
   const [workouts, setWorkouts] = useState([])
   const [tags, setTags] = useState([])
   const [loggedIn, setLoggedIn] = useState(false)
@@ -56,14 +56,36 @@ function App() {
 
     const handleAddWorkout = (newWorkout) => {
       setWorkouts([...workouts, newWorkout])
+      setUser({...user, workouts: [...user.workouts,newWorkout]})
     }
+    //
 
     const handleDeleteWorkout = (id) => {
       const updatedWorkouts = workouts.filter((deletedWorkout) => deletedWorkout.id !== id)
+      const updatedUserWorkouts = user.workouts.filter((deletedUserWorkout) => deletedUserWorkout.id !== id)
+      console.log(typeof id)
+      console.log(updatedUserWorkouts)
+      setUser({...user, workouts: updatedUserWorkouts})
       setWorkouts(updatedWorkouts)
+     
     }
     
-
+    const handleEditWorkout = (updatedWorkout) => {
+      const updatedWorkouts = workouts.map(workout => {
+        if (workout.id === updatedWorkout.id) {
+          return updatedWorkout; // Replace the edited workout with the updated one
+        }
+        return workout;
+      });
+      const updatedUserWorkouts = user.workouts.map(userWorkout => {
+        if (userWorkout.id === updatedWorkout.id) {
+          return updatedWorkout; // Replace the edited workout with the updated one
+        }
+        return userWorkout;
+      })
+      setWorkouts(updatedWorkouts);
+      setUser({...user, workouts: updatedUserWorkouts})
+    };
 
   return (
     <div className="App">
@@ -73,9 +95,9 @@ function App() {
           <Route path="/" element={<Home workouts={workouts} tags={tags} />} />
           <Route path="/login" element={<Login onLogin={setUser} setLoggedIn={setLoggedIn}/>} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/workouts" element={<Workouts workouts={workouts} setWorkouts={setWorkouts} handleAddWorkout={handleAddWorkout} tags={tags} setTags={setTags}/>} />
+          <Route path="/workouts" element={<Workouts loggedIn={loggedIn} workouts={workouts} setWorkouts={setWorkouts} handleAddWorkout={handleAddWorkout} tags={tags} />} />
           <Route path="/myWorkouts" element={<UserWorkouts  />} />
-          <Route path="/workouts/:id" element={<WorkoutShow handleDeleteWorkout={handleDeleteWorkout} />} />
+          <Route path="/workouts/:id" element={<WorkoutShow handleDeleteWorkout={handleDeleteWorkout} handleEditWorkout={handleEditWorkout} tags={tags}/>}  />
           <Route path="/tags/:id" element={<TagShow  />} />
         </Routes>
       </Router>
