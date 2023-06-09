93bdc1191947808e2880a62b057aa90fa8a89e9b
Victoria Parry <vlparry@gmail.com>
2023-06-09 13:10:28 -0400

    attempt to fix CORS but problem with youtube api

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com'
+    origins 'localhost:4000'  # Update the origin to match your front-end address
 
     resource '*',
       headers: :any,
